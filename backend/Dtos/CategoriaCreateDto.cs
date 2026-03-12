b686b31056dce9bad4e3a42e7ec577bf3313529c
WillianXG <wilian.mine789@gmail.com>
2026-03-12 16:27:00 -0300

    Ajustes Backend

---
+++ backend/Dtos/CategoriaCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using ControleGastos.Api.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Api.Dtos
+{
+    public class CategoriaCreateDto
+    {
+        [Required]
+        [MaxLength(400)]
+        public string Descricao { get; set; } =string.Empty;
+
+        [Required]
+        public FinalidadeCategoria Finalidade { get; set; }
+    }
+}
---
+++ backend/Dtos/TransacaoCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using ControleGastos.Api.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Api.Dtos
+{
+    public class TransacaoCreateDto
+    {
+        [Required]
+        [MaxLength(400)]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        [Required]
+        public TipoTransacao Tipo { get; set; }
+
+        [Required]
+        public int PessoaId { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+    }
+}
